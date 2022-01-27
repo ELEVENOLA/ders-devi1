6e8bae02d773a02a9c9876dcae2db80b350ea704
ELEVENGERS <95119844+ELEVENOLA@users.noreply.github.com>
2022-01-27 15:34:33 +0300

    Create ödev2.c

---
+++ ödev2.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+
+
+
+int main( void )
+{
+ int x;
+ int y;
+
+ printf("Sayilari giriniz\n");
+
+scanf("%d", &x);
+scanf("%d", &y);
+
+
+printf("toplama islemi\n\n");
+printf("%d\n\n",x+y);
+printf("cikarma islemi\n\n");
+printf("%d\n\n",x-y);
+printf("bolme islemi\n\n");
+printf("%d\n\n",x%y);
+printf("carpma islemi\n\n");
+printf("%d\n\n",x*y);
+
+
+
+
+
+
+
+
+
+
+}
+

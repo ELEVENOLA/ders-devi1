73c171054115f473c39b0062124c1f1beeea303f
ELEVENGERS <95119844+ELEVENOLA@users.noreply.github.com>
2022-01-28 14:58:20 +0300

    Create ödev3.c

---
+++ ödev3.c
@@ -0,0 +1,35 @@
+
+#include<stdio.h>
+
+
+
+
+int main()
+{
+float height;
+int age;
+char name[20];
+
+printf("ismin ne:");
+scanf("%s",&name);
+printf("Merhaba %s\n\n",name);
+
+
+printf("kac yasindasin?:");
+scanf("%d",&age);
+printf("%d mi daha yaslanmamissin\n\n",age);
+
+
+printf ("boyun kac:");
+scanf("%f",&height);
+printf("%.2f ooo cok uzunsun\n\n",height);
+
+
+
+
+
+
+
+return 0;
+
+}

c69ad81eac8f351640d0f5ef15c9f1111abfa2d0
ELEVENGERS <95119844+ELEVENOLA@users.noreply.github.com>
2022-01-28 16:57:50 +0300

    Create ödev4.c

---
+++ ödev4.c
@@ -0,0 +1,31 @@
+
+#include<stdio.h>
+
+
+
+
+int main()
+{
+int x;
+
+printf("sayinizi girin:");
+scanf("%d",&x);
+
+
+
+if (x%2 ==0){
+
+
+printf("Sayiniz cifttir");
+
+
+
+}
+else {
+
+printf("Sayiniz tektir");
+
+
+}
+return 0;
+}
